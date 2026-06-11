3d59522b0c8ca3c39b943ca50b7d544f03d10d44
Luis E Maldonado Pinto <Luisernestomaldonadopinto@MacBook-Pro-7.local>
2026-06-11 18:29:49 +0200

    Fix supabase hardcoded keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://ldoprjdebsydkfhhecrv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxkb3ByamRlYnN5ZGtmaGhlY3J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNzQ1ODUsImV4cCI6MjA5Njc1MDU4NX0.YWavIx5_ZD1WXUrQA8TEVXjp1VBSU3PpCYiEGxXks3Q'
 )
